d4f9c5520f24f1b736f0f8c16429519b96ec8459
ddave592 <david.g1591@googlemail.com>
2019-12-16 00:14:13 +0000

    cardUi testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './styles/App.scss';
+import CardUi from '../src/components/CardUi'
 
 function App() {
   return (
     <>
+    <CardUi />
     <p>
       Hello World 👋
     </p>
