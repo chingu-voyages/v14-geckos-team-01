7fc90f44d5604fd9d4d87011a0bc8a18125f900a
dgomes <david.g1591@googlemail.com>
2019-12-17 14:39:31 +0000

    reformated, similifed info

--- src/components/CardUi.js
+++ src/components/CardUi.js
@@ -6,20 +6,23 @@ import Collapse from '@material-ui/core/Collapse';
 import '../styles/cardUi.scss'
 import img from '../img/nandos/butterflybreasts.jpg'
 import Chip from '@material-ui/core/Chip';
+import RoomIcon from '@material-ui/icons/RoomRounded';
+// import AttachMoneyIcon from '@material-ui/icons/AttachMoneyRounded';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMoreRounded';
+import CardContent from '@material-ui/core/CardContent';
+import { IconButton } from '@material-ui/core';
+
 
 const CardUi = (props) => {
 
-  // const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = React.useState(false);
 
-  // const handleExpandClick = () => {
-  //   setExpanded(!expanded);
-  // };
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
     <div className="card-container">
-      <h1>
-        Nandos
-  </h1>
       <Card className="main-card">
         <div className="card-img">
           <CardMedia
@@ -28,39 +31,48 @@ const CardUi = (props) => {
             title="nandos"
           />
         </div>
+        <h1 className="card-title">
+          Nandos
+          </h1>
         <div className="info-container">
+          <div className="short-info">
+            <RoomIcon />
+            <span>
+              0.3 miles, Brighton
+            </span>
+            <Chip label="$$" className="card-cost" variant="outlined" />
+          </div>
+
           <div className="card-row">
-
-            <div className="address-container">
-
-              <h2>Address</h2>
-              <p>
-                12 Fried Chicken Avenue<br />
-                Hempdon Park<br />
-                Brighton<br />
-                JF9 8JN
-            </p>
-            </div>
-            <div>
-              <h2>Tags</h2>
-              <div className="chip-container">
-                <Chip label="Chicken" className="chip" onClick={undefined} clickable={true} />
-                <Chip label="Trendy" className="chip" onClick={undefined} clickable={true} />
-                <Chip label="Grilled" className="chip" onClick={undefined} clickable={true} />
-                <Chip label="Portugese" className="chip" onClick={undefined} clickable={true} />
-                <Chip label="Finger-licking" className="chip" onClick={undefined} clickable={true} />
-              </div>
+            <div className="chip-container">
+              <Chip label="Chicken" className="chip" onClick={undefined} clickable={true} />
+              <Chip label="Trendy" className="chip" onClick={undefined} clickable={true} />
+              <Chip label="Grilled" className="chip" onClick={undefined} clickable={true} />
+              <Chip label="Portugese" className="chip" onClick={undefined} clickable={true} />
+              <Chip label="Finger-licking" className="chip" onClick={undefined} clickable={true} />
             </div>
           </div>
-          <div className="card-row">
-            <div>
-              <h2>Price</h2>
-            </div>
-            <div>
-              <h2>Description</h2>
-            </div>
+          <div>
+            <IconButton
+              className={clsx('expand', {
+                'expand': expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
           </div>
         </div>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <h2>Description</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+          </CardContent>
+        </Collapse>
       </Card>
     </div>
   );
