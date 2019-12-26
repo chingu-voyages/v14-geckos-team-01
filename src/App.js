41a1ceb9068b9a91149e26b3cd4c86a38fb73888
David Gomes <hello@davegomes.co.uk>
2019-12-26 19:12:10 +0000

    Revert "Feature card ui component"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import CardUi from '../src/components/CardUi'
-import NavBar from './components/navbar';
 import './styles/App.scss';
 
 function App() {
   return (
     <>
-    <NavBar />
-    <CardUi />
+    <p>
+      Hello World 👋
+    </p>
     </>
   );
 }
