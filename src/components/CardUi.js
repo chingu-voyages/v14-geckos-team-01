ff76f4ad77e15099d58b0555e8f6a6783a77461d
ddave592 <david.g1591@googlemail.com>
2020-01-02 19:06:09 +0000

    styling tests

--- src/components/CardUi.js
+++ src/components/CardUi.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
-import { IconButton, CardContent, Chip, CardMedia, Collapse, Card } from '@material-ui/core';
+import {
+  IconButton,
+  CardContent,
+  Chip,
+  CardMedia,
+  Collapse,
+  Card,
+} from '@material-ui/core';
 import {
   StarRounded as StarRoundedIcon,
   Room as RoomIcon,
   ExpandMore as ExpandMoreIcon,
-  StarBorderRounded as StarBorderRoundedIcon
+  StarBorderRounded as StarBorderRoundedIcon,
+  ClearRounded as ClearAllIcon,
+  DoneOutlineRounded as DoneOutlineRoundedIcon
 } from '@material-ui/icons';
 import clsx from 'clsx';
 import Rating from 'react-rating';
@@ -41,12 +50,11 @@ const CardUi = ({ list }) => {
             <h1>
               {capFirstLetter(list[cardIndex].name)}
             </h1>
-            <div className="break" />
             <div className="short-info">
-                <RoomIcon />
-                <span>
-                  {list[cardIndex].location}
-                </span>
+              <RoomIcon />
+              <span>
+                {list[cardIndex].location}
+              </span>
               <Chip label={'$'.repeat(list[cardIndex].price)} className="card-cost" color="secondary" size="medium" variant="outlined" />
               <Chip label={list[cardIndex].waitTime} className="wait-time" color="primary" size="medium" variant="outlined" />
               <Rating
@@ -66,28 +74,35 @@ const CardUi = ({ list }) => {
                 })}
               </div>
             </div>
-            <div className="card-row mx-auto justify-content-center">
-              <IconButton
-                className={clsx('expand', {
-                  'expand-open': expanded,
-                })}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </IconButton>
-            </div>
           </div>
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-              <h2>{capFirstLetter(list[cardIndex].titleDecription)}</h2>
-              <p>
-                {list[cardIndex].description}
-              </p>
-            </CardContent>
-          </Collapse>
         </div>
+
+        <div className="card-row mx-auto justify-content-center">
+          <IconButton
+            className={clsx('expand', {
+              'expand-open': expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </div>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <h2>{capFirstLetter(list[cardIndex].titleDecription)}</h2>
+            <p>
+              {list[cardIndex].description}
+            </p>
+          </CardContent>
+        </Collapse>
+
+        <div className="card-row card-button-group">
+          <button className="btn btn-decline btn-lg"><ClearAllIcon fontSize='large' /></button>
+          <button className="btn btn-accept btn-lg"><DoneOutlineRoundedIcon fontSize="large" /></button>
+        </div>
+
       </Card>
     </div>
   );
