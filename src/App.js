44ec1cc90fc2956d71176b4f9c4f17c41d3519a3
bonno42h <bonno42h@gmail.com>
2020-01-09 01:07:23 +0200

    Revert "Merge pull request #7 from chingu-voyages/revert-6-refactor/code-style"
    This reverts commit 117238eb57b5ee97fe63375169ae95128d42bda1, reversing
    changes made to 043b32785aa71b6cf1a61a4f6a9deb17e3029395.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import CardUi from './components/CardUi'
-import NavBar from './components/Navbar';
-import './styles/App.scss';
-import restaurants from './data/restaurants.json';
+import restaurants from 'data/restaurants.json';
+import CardUi from 'components/CardUi';
+import NavBar from 'components/Navbar';
+import 'styles/App.scss';
 
 function deepCopy(data) {
   return JSON.parse(JSON.stringify(data));
 }
 
 function ShuffleArr(input) {
-
-  for (let i = input.length - 1; i >= 0; i--) {
-
-    let randomIndex = Math.floor(Math.random() * (i + 1));
-    let itemAtIndex = input[randomIndex];
+  for (let i = input.length - 1; i >= 0; i -= 1) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    const itemAtIndex = input[randomIndex];
 
     input[randomIndex] = input[i];
     input[i] = itemAtIndex;
